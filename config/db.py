acb5b0021310bf1823049f2aa4666bbefbbbc8b2
JavierRamirezPulido <javier.ramirez-pulido@t-systems.com>
2023-01-27 14:10:04 +0100

    Fix mongo pwd

--- config/db.py
+++ config/db.py
@@ -1,3 +1,3 @@
 from pymongo import MongoClient
 
-conn = MongoClient("mongodb+srv://xavivi2000:preguntados2.@t-rivial-api.hwjttpy.mongodb.net/?retryWrites=true&w=majority")
+conn = MongoClient("mongodb+srv://xavivi2000:Preguntados2.@t-rivial-api.hwjttpy.mongodb.net/?retryWrites=true&w=majority")
