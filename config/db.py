859db3aebd96731ccce4327398145115bcf8661a
JavierRamirezPulido <javier.ramirez-pulido@t-systems.com>
2023-01-27 14:20:51 +0100

    mongo

--- config/db.py
+++ config/db.py
@@ -1,3 +1,4 @@
 from pymongo import MongoClient
 
-conn = MongoClient("mongodb+srv://xavivi2000:Preguntados2.@t-rivial-api.hwjttpy.mongodb.net/?retryWrites=true&w=majority")
+client = MongoClient("mongodb+srv://xavivi2000:Preguntados2.@t-rivial-api.hwjttpy.mongodb.net/?retryWrites=true&w=majority")
+conn = client['prod-db']
